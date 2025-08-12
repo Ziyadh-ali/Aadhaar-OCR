4fda09e0fcb34fe516da9752b2f83a9570ed6136
Ziyadh-ali <ziyadhalimodern1010@gmail.com>
2025-08-12 15:01:54 +0530

    test commit4

--- backend/src/config/multer.ts
+++ backend/src/config/multer.ts
@@ -1,9 +1,15 @@
 import multer from "multer";
 import path from "path";
+import fs from "fs";
+
+const uploadPath = path.join(__dirname, "../uploads");
+if (!fs.existsSync(uploadPath)) {
+    fs.mkdirSync(uploadPath, { recursive: true });
+}
 
 const storage = multer.diskStorage({
     destination: (req, file, cd) => {
-        cd(null, path.join(__dirname, "../uploads"))
+        cd(null, uploadPath);
     },
     filename: (req, file, cb) => {
         cb(null, `${Date.now()}-${path.extname(file.originalname)}`);
